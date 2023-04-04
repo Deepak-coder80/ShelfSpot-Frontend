37c47c01cb19c9af02728ecea7216e437f692be0
Deepak M S <deepakcoder80@gmail.com>
2023-04-04 19:29:26 +0530

    updated login and sign up components and login page

--- lib/screens/authenticationScreens/login_page.dart
+++ lib/screens/authenticationScreens/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shelfspot/components/login_button.dart';
-import 'package:shelfspot/components/signup_text_button.dart';
+import 'package:shelfspot/components/buttonComponents/login_button.dart';
+import 'package:shelfspot/components/buttonComponents/signup_text_button.dart';
+import 'package:shelfspot/screens/authenticationScreens/admin_login_page.dart';
 
 class LogInPage extends StatefulWidget {
   const LogInPage({Key? key}) : super(key: key);
@@ -15,36 +16,44 @@ class _LogInPageState extends State<LogInPage> {
     return Scaffold(
       backgroundColor: const Color(0xFF201F15),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/applogo.png'),
-            const Text(
-              'Shelf Spot',
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 36,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/applogo.png'),
+              const Text(
+                'Shelf Spot',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 36,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 52,
-            ),
-            const LoginButton(
-              text: 'Login As Admin',
-            ),
-            const SizedBox(
-              height: 52,
-            ),
-            const LoginButton(
-              text: 'Login As Student',
-            ),
-            const SizedBox(
-              height: 52,
-            ),
-            const SignUpTButton()
-          ],
+              const SizedBox(
+                height: 52,
+              ),
+              LoginButton(
+                text: 'Login As Admin',
+                onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const AdminLoginPage()));
+                },
+              ),
+              const SizedBox(
+                height: 52,
+              ),
+              LoginButton(
+                text: 'Login As Student',
+                onPressed: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const AdminLoginPage()));
+                },
+              ),
+              const SizedBox(
+                height: 52,
+              ),
+              const SignUpTButton()
+            ],
+          ),
         ),
       ),
     );
